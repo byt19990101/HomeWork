ef071274f914008ac50026dcf2d23d3c19da55d7
byt19990101 <byt9911@qq.com>
2020-07-22 11:39:50 +0800

    1.0.0 day1 Test04完成

--- src/day01/Test04.java
+++ src/day01/Test04.java
@@ -1,4 +1,7 @@
 package day01;
+
+import java.util.Scanner;
+
 /**
  * 要求用户从控制台输入一个email地址，然后获取该email的用户名(@之前的内容)
  * @author Bonnie
@@ -6,6 +9,11 @@ package day01;
  */
 public class Test04 {
 	public static void main(String[] args) {
+		System.out.println("请输入email地址：");
+		Scanner sc = new Scanner(System.in);
+		String email = sc.next();
+		int x = email.indexOf("@");
+		System.out.println("该email的用户名为："+email.substring(0,x));
 		
 	}
 }
